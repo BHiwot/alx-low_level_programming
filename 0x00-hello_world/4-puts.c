6031252159f39f433cb47843accbfc7e6d1dc121
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-08 18:15:50 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-/*
- *  main - Entry
- *  Return:  0 
- */
+/**
+ *
+ *  * Description: main - Prints out a string.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
