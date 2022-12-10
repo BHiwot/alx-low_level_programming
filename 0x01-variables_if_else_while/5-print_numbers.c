c79a73196b35ebca7754cd608269191c71069b67
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-09 19:17:16 -0800

    Print base ten numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Print base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; ch <= 9 ; i++)
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
