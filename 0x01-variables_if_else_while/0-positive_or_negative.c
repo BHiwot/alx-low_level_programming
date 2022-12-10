2d968d29005e620979de0efd1204e6ab3b22ad05
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-09 16:47:45 -0800

    Print postive or negetive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include<time.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return :Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -20,6 +20,5 @@ int main(void)
 	printf("%d is positive\n", n);
 	else
 	printf("%d is negative\n", n);
-	
 	return (0);
 }
