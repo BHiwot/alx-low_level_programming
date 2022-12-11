34a400a6e2c8edc0c25b38ab4944b67c65f7a7a5
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-11 05:56:59 -0800

    Print Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar((i % 10) + '0'); 
+	putchar((i % 10) + '0'); 
 	for (i = 'a' ; i <= 'f' ; i++)
-		putchar(i);
+	putchar(i);
 	putchar('\n');
 	return (0);
 }
