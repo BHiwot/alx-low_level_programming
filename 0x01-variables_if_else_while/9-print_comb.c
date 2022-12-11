20a440c96c1f8a4e248234e8b554e9c3a16f889f
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-11 08:03:18 -0800

    prints all possible combinations of single digit number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,6 +9,16 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i", ");
+	{
+		purchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		
+		purchar(',');
+		purchar(' ');
+	}
+
+	purchar('\n');
+	
 	return (0);
 }
