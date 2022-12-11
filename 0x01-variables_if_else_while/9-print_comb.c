501c54bc7da662c92b350a48e07fa73bc7902383
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-11 07:18:16 -0800

    prints all possible combinations of single digit number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include<stdio>
+/**
+ * main - Print all combination of single digit number
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i,", ");
+	Return (0);
+}
