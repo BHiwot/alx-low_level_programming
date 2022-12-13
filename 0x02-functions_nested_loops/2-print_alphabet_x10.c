438475c3e814ef9697641700a940cd628d8ec68a
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-13 11:22:59 -0800

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@ void print_alphabet_x10(void)
 {
 	char ch = 'a';
 	int i = 0;
-
-	while (ch <= 'z')
+	while (i < 10)
 	{
-		while (i < 10)
+		while (ch <= 'z')
 		{
 		_putchar(ch);
 		ch++;
 		}
-	}
 	_putchar('\n');
+	i++;
+	}
 }
