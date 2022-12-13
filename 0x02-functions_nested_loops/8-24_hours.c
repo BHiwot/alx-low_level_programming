67c68aa6a31a62004ee5607b28105bdbdd777571
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-13 14:05:12 -0800

    print 24 hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - beginning
- * 
+ * hr = hour mn minutes
  * Return: 24 hour line by line
  */
 
@@ -11,7 +11,7 @@ void jack_bauer(void)
 
 	for (hr = 0 ; hr < 24 ; hr++)
 	{
-		for(mn = 0 ; mn < 60 ; mn++)
+		for (mn = 0 ; mn < 60 ; mn++)
 		{
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0'); 
