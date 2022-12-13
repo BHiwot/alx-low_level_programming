23a3206b1bb10bdfba82196b0d8165f4eba96c4a
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-13 14:13:27 -0800

    print 24 hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 		for (mn = 0 ; mn < 60 ; mn++)
 		{
 			_putchar((hr / 10) + '0');
-			_putchar((hr%10) + '0'); 
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar((mn / 10) + '0'); 
-			_putchar((mn % 10) + '0'); 
+			_putchar((mn / 10) + '0');
+			_putchar((mn % 10) + '0');
 			_putchar('\n');
 		}
 	}
