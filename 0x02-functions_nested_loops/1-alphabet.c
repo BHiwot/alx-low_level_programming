3d7576203195f82ad402430d4250537a8c23e231
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-13 10:31:01 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - beginning
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
