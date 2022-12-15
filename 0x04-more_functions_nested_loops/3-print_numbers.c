f102bb349b901e8ea3922596ac2f59d5f0c4676a
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-15 12:04:52 -0800

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c = 0;
+
+	while(c <= 9)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
