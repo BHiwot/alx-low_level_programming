1bbdd96eb5e4af1d63e97d524eb1a2876ea676d0
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-19 10:04:08 -0800

    Print string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,19 @@
  * @s: pointer parameter
  * Return: 0
  */
-void print_rev(char *s)
+void print n = strlint_rev(char *s)
 {
-	ln = puts(s);
-	strrev(ln);
+	int g, numb;
+	int tmpry = 0;
+
+	for(numb=0; strg[numb] != 0; numb++);
+	for(g = 0; g <numb/2; g++)
+	{
+		tmpry = strg[g];
+		strg[g]=strg[numb - 1 - g];
+		strg[numb - 1 - g] = tmpry;
+	}
+	for(g = 0; g < numb; g++)
+		putchar(strg[g]);
+	printf(" \n ");
 }
