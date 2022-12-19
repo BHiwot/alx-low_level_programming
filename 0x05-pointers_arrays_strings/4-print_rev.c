c2a067de89674b2545b73596c7d4c81384d9e11e
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-19 10:15:46 -0800

    Print string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@ void print_rev(char *s)
 	int g, numb;
 	int tmpry = 0;
 
-	for(numb=0; strg[numb] != 0; numb++);
+	for(numb=0; s[numb] != 0; numb++);
 	for(g = 0; g <numb/2; g++)
 	{
-		tmpry = strg[g];
-		strg[g]=strg[numb - 1 - g];
-		strg[numb - 1 - g] = tmpry;
+		tmpry = s[g];
+		s[g]=s[numb - 1 - g];
+		s[numb - 1 - g] = tmpry;
 	}
 	for(g = 0; g < numb; g++)
-		putchar(strg[g]);
+		putchar(s[g]);
 	printf(" \n ");
 }
