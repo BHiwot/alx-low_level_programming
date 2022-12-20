f01752155327ee429abb0905968dc6bbf4adda50
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-19 18:47:37 -0800

    print n element of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 
 	for (x = 0; x < (n - 1); x++)
 		printf("%d, ", a[x]);
-		if (x == (n - 1))
-			printf("%d", a[n - 1]);
+	if (x == (n - 1))
+		printf("%d", a[n - 1]);
 	printf("\n");
 }
