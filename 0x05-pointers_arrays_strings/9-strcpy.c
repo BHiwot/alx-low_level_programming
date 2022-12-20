314b2c04d2fc0cdeb94e3f0ab6dc35f5cf036535
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-19 19:07:00 -0800

    copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,11 +7,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int a = 0;
+	int a;
 
-	for (; src[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 	{
 		dest[a] = src[a];
 	}
+	dest[a] = '\0';
 	return (dest);
 }
