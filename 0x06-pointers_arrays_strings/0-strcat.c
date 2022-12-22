9b759a7479cfa38ad6c3adda174a4a0dfe0cd040
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-21 18:15:42 -0800

    concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcat - function concatenates two string
+ * @dest: first input
+ * @src: second input
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	int j = strlen(s);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i + j] = s1[i];
+	}
+	s[i + j] = '\0';
+	return (0);
+}
