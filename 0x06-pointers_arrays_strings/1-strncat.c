a181ae174d2536fc2416b48305187a6c13f4646d
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-21 22:18:06 -0800

    concatenate to string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *s1 = dest;
-	while (*s1 != '\0')
+	while (*dest != '\0')
 	{
-		s1++;
+		dest++;
 	}
 	while (*src != '\0' && n--)
 	{
-		*s1++ = *src++;
+		*dest++ = *src++;
 	}
-	*s1 = '\0';
-	return (s1);
+	*dest = '\0';
+	return (dest);
 }
