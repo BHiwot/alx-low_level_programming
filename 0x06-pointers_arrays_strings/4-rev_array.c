e52d10ece26f98cc6e13b375813d106a3417726b
Hiwot Berhanu <hmmmfb@gmail.com>
2022-12-22 05:10:07 -0800

    Reverse String

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reverse_array - function reverse the content of array
+ * @a: first input
+ * @n: second input
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	for (int start = 0, end = n - 1 ; start < end; start++, end++)
+	{
+		int temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+	}
+}
