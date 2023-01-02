efc197b2ba4108f2940d84176a6a27c17e09b9d2
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-02 06:00:14 -0800

    locate char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int a;
+
+	for (a = 0; s[i] >= '\0'; a++)
 	{
-		if (*s == c)
-		{
-			return (char *) s;
-		}
-		s++;
+		if (s[a] == c)
+			return (s + a);
 	}
 	return (NULL);
 }
