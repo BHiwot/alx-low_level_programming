3fece2ec73e9fea39455763e3582b43c1b453572
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-02 03:44:25 -0800

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_memset -  function fill memory
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
