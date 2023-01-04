ae0fc793643206161534e50e0efa92d4c0554ed0
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-04 05:09:49 -0800

    Length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,8 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == NULL)
 	{
-		return (_strlen_recursion(s + 1)) + 1;
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
