d4de4499b65d4f2136966605149ebc5161efbcf0
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-04 05:34:01 -0800

    Power of

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - return x raised to power of y
+ * @x: input
+ * @y: input
+ * Return: return -1 if n is less than 0 else 1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_pow_recursion + 1);
+	}
+}
