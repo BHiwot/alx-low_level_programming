611d30ae9cf0190042984d6234078d0f2bf72509
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-04 03:11:56 -0800

    Print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that print string
+ * @s: input
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
