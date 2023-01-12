0e6a73357ea222daf17e08b4042cce3d1201f918
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-12 05:28:52 -0800

    concatenate

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _calloc - unction allocates memory
+ * @nmemb: element
+ * @size: size byte
+ * Return: NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * array_range -  function that creates an array of integers
+ * @min: min
+ * @max: max
+ * Return: NULL
+ */
+int *array_range(int min, int max)
+{
+
+}
