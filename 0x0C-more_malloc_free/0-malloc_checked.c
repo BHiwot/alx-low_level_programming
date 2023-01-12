17d67d0818e3af7bfc7102c02cf3f9432f492298
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-12 04:48:52 -0800

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory
+ * @b: the size to allocate
+ * Return: pointer to allocated memory or 98 if failed.
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
