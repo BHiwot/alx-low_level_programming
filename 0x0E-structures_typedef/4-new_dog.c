f281afe2e5ce00f2c66114da5c45deb3a0e13119
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-17 18:15:35 -0800

    free dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+int _strlen(char *s);
+/**
+ * *new_dog - ...
+ * @name: ..
+ * @age: ..
+ * @owner: ..
+ * Return: ..
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
