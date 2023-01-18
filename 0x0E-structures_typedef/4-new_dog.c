fbb1247773ff69977484b7eb9e5e9298d67df004
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-17 18:34:39 -0800

    creat new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,9 @@
 #include "dog.h"
 #include <stdio.h>
 int _strlen(char *s);
+char *_strcpy(char *x, char *y);
 /**
- * *new_dog - ...
+ * new_dog - ...
  * @name: ..
  * @age: ..
  * @owner: ..
@@ -10,10 +11,64 @@ int _strlen(char *s);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
+	if (name == NULL)
+		new_d->name = NULL;
+	else
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		new_d->name = malloc(_strlen(name) + 1);
+		if (new_d->name == NULL)
+		{
+			free(new_d);
+			return (NULL);
+		}
+		new_d->name = _strcpy(new_d->name, name);
 	}
+	if (owner == NULL)
+		new_d->owner = NULL;
+	else
+	{
+		new_d->owner = malloc(_strlen(owner) + 1);
+		if (new_d->owner == NULL)
+		{
+			free(new_d->name);
+			free(new_d);
+			return (NULL);
+		}
+		new_d->owner = _strcpy(new_d->owner, owner);
+	}
+	new_d->age = age;
+	return (new_d);
+}
+/**
+ * _strlen - gets len of str
+ * @s: ..
+ * Return: ..
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * _strcpy: ..
+ *  @x: pointer to destination of string
+ *  @y: ..
+ *  Return: pointer to dest
+ */
+char *_strcpy(char *x, char *y)
+{
+	char *a = x;
+
+	while (*y)
+		*a++ = *y++;
+	*a = '\0';
+	return (x);
 }
