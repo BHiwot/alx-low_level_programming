f3beed7d292285b3758d8be52de2416f39f48ce0
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-19 17:46:14 -0800

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(vl, char *);
-		if (num == NULL)
+		if (num == 0)
 		{
 			printf("%s", "(nil)");
 		}
