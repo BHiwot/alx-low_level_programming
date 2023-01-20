10b7b7c1f0a20bff4903e7e82a2ab1e9e62558ba
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-19 16:21:28 -0800

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - ...
+ * @n: ..
+ * Return: ..
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list vl;
+	unsigned int a;
+
+	if (n != 0)
+	{
+		va_start(vl, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(vl, int);
+		}
+		va_end(vl);
+		return (sum);
+	}
+	return (0);
+}
