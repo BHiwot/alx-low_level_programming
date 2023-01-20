8797577b1ac405ac8539a052f697479e258864bd
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-19 17:56:19 -0800

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	va_end(vl);
 	printf("\n");
+	va_end(vl);
 }
