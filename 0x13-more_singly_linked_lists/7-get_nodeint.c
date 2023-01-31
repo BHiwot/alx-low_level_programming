60a7db497dc4020cbebd64f0de527ec01e4abb24
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-30 20:53:58 -0800

    locate given node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - return nth node
+ * @head: ..
+ * @index: ..
+ * return: nth node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node = 0;
+
+	while (node < index)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+		node++;
+	}
+	return (head);
+}
