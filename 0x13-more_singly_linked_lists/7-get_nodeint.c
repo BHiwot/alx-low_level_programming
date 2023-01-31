e4750cc326e25f20daa1c27b31dbaface453d80e
Hiwot Berhanu <hmmmfb@gmail.com>
2023-01-30 20:55:13 -0800

    locate given node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * get_nodeint_at_index - return nth node
  * @head: ..
  * @index: ..
- * return: nth node or NULL
+ * Return: nth node or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
